410f2a0d5272f012300151b6d492748d271fd19b
Ricardo Chavarria <projetoPlantal2018@gmail.com>
2019-04-03 01:41:00 +0100

    Tarefas Ricardo Chavarria

--- app/src/main/java/com/example/chuch/trabalho/Inicio.java
+++ app/src/main/java/com/example/chuch/trabalho/Inicio.java
@@ -1,5 +1,10 @@
 package com.example.chuch.trabalho;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +17,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
 
 public class Inicio extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +30,6 @@ public class Inicio extends AppCompatActivity
         setContentView(R.layout.activity_inicio);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -74,7 +80,8 @@ public class Inicio extends AppCompatActivity
         if (id == R.id.consultarDoacoes) {
             // Handle the camera action
         } else if (id == R.id.proximaDoacao) {
-
+            Intent intent = new Intent(getApplicationContext(), Notificacao.class);
+            startActivity(intent);
         } else if (id == R.id.direitos) {
 
         } else if (id == R.id.certificado) {
--- app/src/main/java/com/example/chuch/trabalho/LoginActivity.java
+++ app/src/main/java/com/example/chuch/trabalho/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.chuch.trabalho;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,18 +17,28 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
+public class LoginActivity extends AppCompatActivity {
+    String URLL, URLLL;
     Button btn_login;
     EditText et_username, et_password;
-    private static String URL_LOGIN = "http://192.168.1.168/trabalho/login.php";
+    private static String URL_DADOS = "http://172.16.184.74/trab/api/id/";
+    private static String URL_LOGIN = "http://172.16.184.74/trab/api/login/";
     private Context mContext;
+    String id;
+    String username;
 
 
     @Override
@@ -38,33 +51,80 @@ public class LoginActivity extends AppCompatActivity {
         et_username = findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
 
+         id="";
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login();
+                verDados();
             }
         });
     }
 
-    public void login() {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
-                new Response.Listener<String>() {
+    public void verDados(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        URLLL = URL_DADOS + et_username.getText().toString();
+        Log.d("URL:", URLLL);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URLLL, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(String response) {
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
 
-                if(response.trim().equals("success")){
-                    Intent intent = new Intent(getApplicationContext(), Inicio.class);
-                    startActivity(intent);
-                    finish();
+                         id = jsonObject.getString("id");
+                         username = jsonObject.getString("username");
+                        //Log.d("LOGID:", id);
+                        //Log.d("LOGUSERNAME", username);
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "Credenciais erradas. Tente novamente", Toast.LENGTH_SHORT).show();
-
+                    } catch (JSONException ex) {
+                        ex.printStackTrace();
+                        progressDialog.dismiss();
+                    }
                 }
-
+                progressDialog.dismiss();
             }
         }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+
+
+
+    }
+
+    public void login() {
+        URLL = URL_LOGIN + et_username.getText().toString()+";"+et_password.getText().toString();
+        Log.d("LINK", URLL);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        if(response.trim().equals("success")){
+                            Intent intent = new Intent(getApplicationContext(), MenuPrincipal.class);
+                            intent.putExtra("id",id);
+                            intent.putExtra("username", username);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Credenciais erradas. Tente novamente", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), "error:" +error.toString(), Toast.LENGTH_SHORT).show();
@@ -75,11 +135,16 @@ public class LoginActivity extends AppCompatActivity {
                 Map<String, String> params = new HashMap<>();
                 params.put("UserName", et_username.getText().toString());
                 params.put("Password", et_password.getText().toString());
+
                 return params;
             }
         };
         requestQueue.add(stringRequest);
 
     }
+
+
+
+
 
 }
